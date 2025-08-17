a97a2176d893e2e080344a42929f92af96c01f43
Mike Mendoza <engr.chaelmendoza@gmail.com>
2025-08-18 06:22:11 +0800

    chore: restore comment to clarify purpose of LoginPage component

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/navigation';
 import styles from './page.module.scss';
 
 export default function LoginPage() {
+  // This page is the login screen for the Cat Care Portal
   const [accessCode, setAccessCode] = useState('');
   const [error, setError] = useState('');
   const [isLoading, setIsLoading] = useState(false);
