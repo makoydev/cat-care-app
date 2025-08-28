a574fff911978afcf0ad7274afc96bfebd70656e
Mike Mendoza <engr.chaelmendoza@gmail.com>
2025-08-28 22:58:57 +0800

    chore: remove simulateApiCall function from LoginPage component

--- app/page.tsx
+++ app/page.tsx
@@ -10,13 +10,7 @@ export default function LoginPage() {
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
   // Simulating an API call
-  const simulateApiCall = () => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(true);
-      }, 2000);
-    });
-  };
+
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
