7e77923f06aa09ba30061bdb0e943fe3c79e0d7b
Mike Mendoza <engr.chaelmendoza@gmail.com>
2025-09-16 16:47:56 +0800

    chore: remove redundant comment for authenticated user redirection in LoginPage

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,6 @@ export default function LoginPage() {
   const router = useRouter();
   const isAuthenticated = sessionStorage.getItem('authenticated') === 'true';
   
-  // If already authenticated, redirect to /cats
   if (isAuthenticated) {
     router.push('/cats');
     return null;
