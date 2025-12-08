1cbaad8e8d6a88aec84b13b6b5a996179e76684d
Mike Mendoza <engr.chaelmendoza@gmail.com>
2025-12-08 11:54:45 +0800

    add test

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,6 @@ export default function LoginPage() {
   const isAuthenticated = sessionStorage.getItem('authenticated') === 'true';
   
   if (isAuthenticated) {
-    //router push
     router.push('/cats');
     //return null
     return null;
